1f0c21c4c31f48802d76e415edd7a21e83b938d2
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-08 13:36:48 +0200

    Pasos de instalacion y trasteo sencillo

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'share-menu',
+  templateUrl: './menu.component.html',
+  styles: [
+  ]
+})
+export class MenuComponent {
+
+ menuItems: MenuItem[] = [];
+
+    ngOnInit() {
+        this.menuItems = [
+            {
+              label: 'Pipes de Angular', icon: 'pi pi-desktop',
+              items:[
+                {
+                  label: "Textos y flechas",
+                  icon: "pi pi-align-left"
+                },
+                {
+                  label: "Numeros",
+                  icon: "pi pi-dollar"
+                },
+                {
+                  label: "No comunes",
+                  icon: "pi pi-globe"
+                }
+              ]
+            },
+            {
+              label: "Pipes pesonalizados", icon: "pi pi-cog",
+              items: [
+                {
+                  label:"Otro elemento",
+                  icon:"pi pi-cog"
+                }
+              ]
+            }
+        ];
+    }
+}
