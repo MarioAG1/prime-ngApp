7080c737c900fa3c858b7db22b51dffd8edbfffa
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-09 13:04:34 +0200

    Cambiar el idioma global y manualmente junto con las fechas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+
+//Configuracion del locale de la app
+import localEs from "@angular/common/locales/es"
+import localFr from "@angular/common/locales/fr"
+import { registerLocaleData } from "@angular/common";
+
+registerLocaleData(localEs)
+registerLocaleData(localFr)
 
 @NgModule({
   declarations: [
@@ -15,7 +23,11 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: "es-ES"
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
