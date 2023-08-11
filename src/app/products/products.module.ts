fa71caea8aae04fd5658f18d707f49568e9d32e0
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-11 15:24:42 +0200

    Aplicacion terminada

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -6,13 +6,25 @@ import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+import { OrderComponent } from './pages/order/order.component';
+
+// Pipes
+import { CanFlyPipe } from './pipes/can-fly.pipe';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+import { SortByPipe } from './pipes/sort-by.pipe';
 
 
 @NgModule({
   declarations: [
     BasicPageComponent,
     NumbersPageComponent,
-    UncommonPageComponent
+    UncommonPageComponent,
+    OrderComponent,
+    //Pipes
+    CanFlyPipe,
+    ToggleCasePipe,
+    SortByPipe,
+
   ],
   imports: [
     CommonModule,
